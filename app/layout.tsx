c12b4b26fc7624ab463da323aea41321d1c5ddfb
NisargSolanki <nisarg.zotech@gmail.com>
2025-11-10 09:43:41 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,6 +38,7 @@ export default function RootLayout({
 
       <body className={`${inter.className} transition-colors duration-300`}>
         <ThemeProvider>{children}</ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
