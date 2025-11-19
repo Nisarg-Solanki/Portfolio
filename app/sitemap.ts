53fef374d2016c983756524b3967405e1ff84918
NisargSolanki <nisarg.zotech@gmail.com>
2025-11-19 18:53:26 +0530

    SEO fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,15 @@
 import { MetadataRoute } from "next";
 
-const domain = process.env.NEXT_PUBLIC_DOMAIN || "";
+const domain =
+  process.env.NEXT_PUBLIC_DOMAIN || "https://nisarg-portfolio.info";
+
+export const revalidate = 60;
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: domain,
+      lastModified: new Date(),
     },
   ];
 }
