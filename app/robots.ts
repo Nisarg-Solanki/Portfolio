9f033030a466af3a397ec36d5476d2b1f3576770
NisargSolanki <nisarg.zotech@gmail.com>
2025-12-14 11:52:49 +0530

    next version update and meta data fix

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,7 @@
 import { MetadataRoute } from "next";
 
-const domain = process.env.NEXT_PUBLIC_DOMAIN || "https://nisarg-portfolio.info";
+const domain =
+  process.env.NEXT_PUBLIC_DOMAIN || "https://nisarg-portfolio.info";
 
 export default function robots(): MetadataRoute.Robots {
   return {
